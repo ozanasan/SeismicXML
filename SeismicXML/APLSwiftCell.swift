6d57a678e4fd942e0c1b0ead160a8ec8ec63c1e4
ozanasan <ozanasan@gmail.com>
2015-02-21 02:05:21 -0800

    added swift class and bridge

---
+++ SeismicXML/APLSwiftCell.swift
@@ -0,0 +1,24 @@
+//
+//  APLSwiftCell.swift
+//  SeismicXML
+//
+//  Created by Ozan Asan on 21/02/15.
+//  Copyright (c) 2015 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class APLSwiftCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
