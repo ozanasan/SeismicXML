8db7e99b87d929ac8ac2b10ce365b297fc884dc9
ozanasan <ozanasan@gmail.com>
2015-02-22 01:21:52 -0800

    Swift Class 1.0

--- SeismicXML/APLSwiftCell.swift
+++ SeismicXML/APLSwiftCell.swift
@@ -14,33 +14,61 @@ class APLSwiftCell: UITableViewCell {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var magnitudeLabel: UILabel!
-    @IBOutlet weak var magnitudeImage: UILabel!
+    @IBOutlet weak var magnitudeImage: UIImageView!
     
-    let dateFormatter : NSDateFormatter!
+    
+    lazy private var dateFormatter : NSDateFormatter = {
+        var temporaryFormat: NSDateFormatter = NSDateFormatter()
+        temporaryFormat.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+        temporaryFormat.dateStyle = NSDateFormatterStyle.MediumStyle
+        temporaryFormat.timeStyle = NSDateFormatterStyle.MediumStyle
+        return temporaryFormat
+    }()
+
+    
     
     func configureWithEarthquake(earthquake :APLEarthquake){
-        self.locationLabel.text = earthquake.location
-        self.dateLabel.text = "/(self.dateFormatter.stringFromDate(arthquake.date))"
-        self.magnitudeLabel.text = "/(earthquake.magnitude)"
-        //self.magnitudeImage.text =
-    }
-    /*
-    func imageForMagnitude(magnitude: CGFloat) -> UIImage{
-        switch
         
+        var tempEarthquakeDate :String = self.dateFormatter.stringFromDate(earthquake.date)
+        
+        self.locationLabel.text = earthquake.location
+        self.dateLabel.text = tempEarthquakeDate
+        self.magnitudeLabel.text = NSString(format: "%.1f", earthquake.magnitude)
+        self.magnitudeImage.image = imageForMagnitude(earthquake.magnitude)
     }
-    */
     
-
+    func imageForMagnitude(magnitude: CGFloat) -> UIImage{
+        
+        if(magnitude >= 5.0){
+            return UIImage(named:"5.0.png")!
+        }
+        else if(magnitude >= 4.0){
+            return UIImage(named:"4.0.png")!
+        }
+        else if(magnitude >= 3.0){
+            return UIImage(named:"3.0.png")!
+        }
+        else if(magnitude >= 2.0){
+            return UIImage(named:"2.0.png")!
+        }
+        return UIImage()
+    }
+    
+    
+    
+    
+    /*
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    */
+    
+    /*
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
-
+    */
 }
